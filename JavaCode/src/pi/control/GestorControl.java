8be16723940ba9d790dee10d2ef2fba0a8273072
Jun Zhou <zhoujun97099@gmail.com>
2022-05-17 22:10:30 +0200

    inicializado la clase control e implementado el constructor

--- JavaCode/src/pi/control/GestorControl.java
+++ JavaCode/src/pi/control/GestorControl.java
@@ -1,5 +1,21 @@
 package pi.control;
 
-public class GestorControl {
+import java.awt.event.ActionListener;
 
+public class GestorControl implements ActionListener{
+	
+	private VInicio vInicio;
+	
+	public GestorControl(VInicio vInicio) {
+		super();
+		this.vInicio = vInicio;
+	}
+	
+	
+	public void actionPerformed(ActionEvent ev ) {
+		
+	}
+	
+	
+	
 }
