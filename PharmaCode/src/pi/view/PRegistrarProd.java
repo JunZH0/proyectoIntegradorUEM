31c9268da163b29a60ea2e9efefe556c9136b59e
Jun Zhou <zhoujun97099@gmail.com>
2022-06-07 11:09:07 +0200

    inicializado design de PRegistrarProd

--- PharmaCode/src/pi/view/PRegistrarProd.java
+++ PharmaCode/src/pi/view/PRegistrarProd.java
@@ -1,7 +1,15 @@
 package pi.view;
 
 import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
 
 public class PRegistrarProd extends JPanel {
+	public PRegistrarProd() {
+		setLayout(new MigLayout("", "[][][][][][][][]", "[][]"));
+		
+		JLabel lblRegistrarProductos = new JLabel("Registrar Productos");
+		add(lblRegistrarProductos, "cell 4 1");
+	}
 
 }
